d88b4b232fc240c80a7c00ff94e14d1c9a48c8b4
bruce.qin <826619176@qq.com>
2019-06-11 22:45:59 +0800

    更新docker镜像地址

--- k8s-node-install.sh
+++ k8s-node-install.sh
@@ -126,7 +126,7 @@ install_docker(){
     mkdir -p /etc/docker
     cat > /etc/docker/daemon.json << EOF
 {
-    "registry-mirrors": ["https://registry.docker-cn.com", "https://mirror.ccs.tencentyun.com", "http://hub-mirror.c.163.com"],
+    "registry-mirrors": ["http://f1361db2.m.daocloud.io", "http://hub-mirror.c.163.com"],
     "insecure-registries": [],
     "exec-opts": ["native.cgroupdriver=cgroupfs"],
     "log-driver": "json-file",
